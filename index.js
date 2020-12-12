6c5d151db546de56adaa85efb9dc5f875bf56ca6
Tyler Bolty <tsbolty@gmail.com>
2020-12-12 16:56:25 -0700

    fully working

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var request = require('request');
  
 var requestSettings = {
   method: 'GET',
-  url: 'https://www.rtd-denver.com/files/gtfs-rt/TripUpdate.pb',
+  url: 'https://www.rtd-denver.com/files/gtfs-rt/Alerts.pb',
   encoding: null
 };
 request(requestSettings, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-    console.log(feed.entity[0].tripUpdate)
-    // feed.entity.forEach(function(entity) {
-    //   if (entity.tripUpdate) {
-    //     console.log(entity.tripUpdate);
-    //   }
-    // });
+    // console.log(feed.entity.alert)
+    feed.entity.forEach(function(entity) {
+      if (entity.alert) {
+        console.log(entity.alert);
+      }
+    });
   } else {
     console.log("nope")
   }
