707202295020d613ecf61587bb541200ddbf83aa
Tyler Bolty <tsbolty@gmail.com>
2020-12-12 16:24:55 -0700

    can console.log trip updates

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ var request = require('request');
  
 var requestSettings = {
   method: 'GET',
-  url: 'URL OF YOUR GTFS-REALTIME SOURCE GOES HERE',
+  url: 'https://www.rtd-denver.com/files/gtfs-rt/TripUpdate.pb',
   encoding: null
 };
 request(requestSettings, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-    feed.entity.forEach(function(entity) {
-      if (entity.trip_update) {
-        console.log(entity.trip_update);
-      }
-    });
+    console.log(feed.entity[0].tripUpdate)
+    // feed.entity.forEach(function(entity) {
+    //   if (entity.tripUpdate) {
+    //     console.log(entity.tripUpdate);
+    //   }
+    // });
+  } else {
+    console.log("nope")
   }
 });
+
